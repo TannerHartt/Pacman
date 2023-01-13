0a51fd81a7b171c7d7030c6ade53175d8dbaff47
Tanner Hart <tannerhart24@yahoo.com>
2023-01-13 12:50:14 -0800

    Added a ternary operator to handle color changing when power up is grabbed. Also added power-ups that allow the player to eliminate ghosts.

--- js/classes.js
+++ js/classes.js
@@ -65,7 +65,7 @@ class Ghost {
 
     draw() {
         ctx.beginPath();
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = this.scared ? 'blue' : this.color;
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
         ctx.fill();
         ctx.closePath();
@@ -75,5 +75,20 @@ class Ghost {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+    }
+}
+
+class PowerUp {
+    constructor({ position }) {
+        this.position = position;
+        this.radius = 8;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.closePath();
     }
 }
