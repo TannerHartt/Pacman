73d9882ae9f57236bed131f726bed1c6415f2b79
Tanner Hart <tannerhart24@yahoo.com>
2023-01-12 17:02:08 -0800

    Abstracted the classes into a separate file to clean up game file

---
+++ js/classes.js
@@ -0,0 +1,51 @@
+class Boundary {
+    static width = 40;
+    static height = 40;
+    constructor({ position, image }) {
+        this.position = position;
+        this.width = Boundary.width;
+        this.height = Boundary.height;
+        this.image = image;
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = 'yellow';
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+class Pellet {
+    constructor({ position }) {
+        this.position = position;
+        this.radius = 3;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = 'yellow';
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
